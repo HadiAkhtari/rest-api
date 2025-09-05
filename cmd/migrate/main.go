1eeebedd3f24ca9cdb63dc04a4c022336b539aab
Hadi <hadi.akhtari1996@gmail.com>
2025-09-06 00:32:54 +0330

    Initial commit: add Go REST API project and migrations

---
+++ cmd/migrate/main.go
@@ -0,0 +1,63 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"github.com/golang-migrate/migrate/v4"
+	"github.com/golang-migrate/migrate/v4/database/postgres"
+	"github.com/golang-migrate/migrate/v4/source/file"
+	_ "github.com/lib/pq"
+	"log"
+	"os"
+)
+
+func main() {
+	if len(os.Args) < 2 {
+		log.Fatal("Please provide a migration direction: 'up' or 'down'")
+	}
+	direction := os.Args[1]
+	connstr := "host=localhost port=5432 user=postgres password=1234 dbname=rest-api sslmode=disable"
+	db, err := sql.Open("postgres", connstr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	instance, err := postgres.WithInstance(db, &postgres.Config{})
+	if err != nil {
+		log.Fatal(err)
+	}
+	fSrc, err := (&file.File{}).Open("migrations")
+	if err != nil {
+		log.Fatal(err)
+	}
+	m, err := migrate.NewWithInstance("file", fSrc, "postgres", instance)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	switch direction {
+	case "up":
+		if err := m.Up(); err != nil {
+			if err == migrate.ErrNoChange {
+				fmt.Println("No new migrations to apply.")
+			} else {
+				log.Fatal(err)
+			}
+
+		} else {
+			fmt.Println("Migrations applied successfully!")
+		}
+	case "down":
+		if err := m.Down(); err != migrate.ErrNoChange {
+			if err == migrate.ErrNoChange {
+				log.Println("No migrations to rollback.")
+			} else {
+				log.Fatal(err)
+			}
+		} else {
+			log.Println("Migrations rolled back successfully!")
+		}
+	default:
+		log.Fatal("Invalid direction. Use 'up' or 'down'.")
+	}
+}
---
+++ go.mod
@@ -0,0 +1,15 @@
+module rest-api-in-gin
+
+go 1.24.3
+
+require (
+	github.com/golang-migrate/migrate/v4 v4.19.0
+	github.com/lib/pq v1.10.9
+)
+
+require (
+	github.com/hashicorp/errwrap v1.1.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	golang.org/x/sys v0.33.0 // indirect
+)
